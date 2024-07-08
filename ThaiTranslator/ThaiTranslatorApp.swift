9df83a1dc2bda539067f4dad95e895a56b54fc9a
Mike haslam <ongomobile@gmail.com>
2024-07-08 10:31:01 -0700

    Initial Commit

---
+++ ThaiTranslator/ThaiTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThaiTranslatorApp.swift
+//  ThaiTranslator
+//
+//  Created by Emorn on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ThaiTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
